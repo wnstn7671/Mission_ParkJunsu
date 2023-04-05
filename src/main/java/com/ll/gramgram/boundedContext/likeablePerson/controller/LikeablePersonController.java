028b1b2df28246f37106534a9bcef7013a6ccd39
wnstn7671 <wnstn7671@naver.com>
2023-04-05 15:11:15 +0900

    init: asd wq!

---
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -0,0 +1,82 @@
+package com.ll.gramgram.boundedContext.likeablePerson.controller;
+
+import com.ll.gramgram.base.rq.Rq;
+import com.ll.gramgram.base.rsData.RsData;
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import com.ll.gramgram.boundedContext.likeablePerson.repository.LikeablePersonRepository;
+import com.ll.gramgram.boundedContext.likeablePerson.service.LikeablePersonService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/likeablePerson")
+@RequiredArgsConstructor
+public class LikeablePersonController {
+    private final Rq rq;
+    private final LikeablePersonService likeablePersonService;
+    private final LikeablePersonRepository likeablePersonRepository;
+
+    @GetMapping("/add")
+    public String showAdd() {
+        return "usr/likeablePerson/add";
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class AddForm {
+        private final String username;
+        private final int attractiveTypeCode;
+    }
+
+    @PostMapping("/add")
+    public String add(@Valid AddForm addForm) {
+        RsData<LikeablePerson> createRsData = likeablePersonService.like(rq.getMember(), addForm.getUsername(), addForm.getAttractiveTypeCode());
+
+        if (createRsData.isFail()) {
+            return rq.historyBack(createRsData);
+        }
+
+        return rq.redirectWithMsg("/likeablePerson/list", createRsData);
+    }
+
+    @GetMapping("/list")
+    public String showList(Model model) {
+        InstaMember instaMember = rq.getMember().getInstaMember();
+
+        // 인스타인증을 했는지 체크
+        if (instaMember != null) {
+            List<LikeablePerson> likeablePeople = likeablePersonService.findByFromInstaMemberId(instaMember.getId());
+            model.addAttribute("likeablePeople", likeablePeople);
+        }
+
+        return "usr/likeablePerson/list";
+    }
+
+//    @PreAuthorize("isAuthenticated()") // 정상작동하지만 service를 도입하고싶다.
+//    @Transactional
+//    @GetMapping("/delete/{id}")
+//    public String LikeDelete(@PathVariable("id") Integer id) {
+//        Optional<LikeablePerson> likeablePerson = likeablePersonRepository.findById(id);
+//        if(likeablePerson.isPresent())
+//        {
+//            LikeablePerson q = likeablePerson.get();
+//            likeablePersonService.delete(q);
+//        }
+//
+//        return rq.redirectWithMsg("/likeablePerson/list", "삭제가 완료되었습니다.");
+//    }
+}
