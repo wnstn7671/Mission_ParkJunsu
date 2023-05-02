3b9871d0c8ed2d48618ca1cdd2b43d7b4a3cd812
wnstn7671 <wnstn7671@naver.com>
2023-05-02 11:45:27 +0900

    호감 사유 수정 및 호감 삭제 서버로직 구현도 완료

--- src/main/java/com/ll/gramgram/GramgramApplication.java
+++ src/main/java/com/ll/gramgram/GramgramApplication.java
@@ -3,6 +3,9 @@ package com.ll.gramgram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableJpaAuditing // @EntityListeners(AuditingEntityListener.class) 가 작동하도록 허용
@@ -13,3 +16,27 @@ public class GramgramApplication {
     }
 
 }
+/*
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+    private static final int CREATE_POST_INTERVAL_MINUTES = 30;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @PostMapping
+    public ResponseEntity<Post> createPost(@RequestBody Post post) {
+        LocalDateTime now = LocalDateTime.now();
+        if (postRepository.findFirstByCreatedTimeAfterOrderByCreatedTimeDesc(now.minusMinutes(CREATE_POST_INTERVAL_MINUTES)).isPresent()) {
+            throw new RuntimeException("Cannot create post within " + CREATE_POST_INTERVAL_MINUTES + " minutes of previous post creation.");
+        }
+        post.setCreatedTime(now);
+        postRepository.save(post);
+        return ResponseEntity.ok(post);
+    }
+}
+
+
+ */
--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -10,6 +10,7 @@ import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -68,6 +69,7 @@ public class LikeablePersonController {
 
         return "usr/likeablePerson/list";
     }
+
 
     @PreAuthorize("isAuthenticated()")
     @DeleteMapping("/{id}")
